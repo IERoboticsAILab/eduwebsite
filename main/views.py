7d7731a1340847fedcff1859c7f85715af2e60da
haxy <zaidsaheb4@gmail.com>
2025-01-13 20:52:56 +0100

    start of edu's website

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def projects(request):
+    return render(request, 'main/projects.html')
